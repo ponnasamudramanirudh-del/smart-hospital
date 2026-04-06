90ce4d40e44aa692b2dd5c7d1682928acfc0b2e2
navo <navo@gmail.com>
2026-04-06 16:23:52 +0530

    updated port for deployment

--- backend.py
+++ backend.py
@@ -134,4 +134,6 @@ def book():
     return jsonify({"reply": "❌ Selected slot not available"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
